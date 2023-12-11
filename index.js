262dc22b380dad12bf0fa2152765c56b1eeac2e5
Marvin <okongomarvin971@gmail.com>
2023-12-11 09:34:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+const names = [];
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderfull ${event} gift!`);
+        debugger;
+      }
+    
+      return names;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown(startingNumber) {
+  if (startingNumber <= 0 || !Number.isInteger(startingNumber)) {
+    console.log("Please provide a positive integer.");
+    return;
+  }
+
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+
+countDown(10);
